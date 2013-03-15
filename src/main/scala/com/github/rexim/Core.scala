1ba0c12831fe020b32829192ed3a4cf9655d2e10
rexim <reximkut@gmail.com>
2013-03-15 17:50:11 +0700

    Core: Started new implementation of unification.

--- src/main/scala/com/github/rexim/Core.scala
+++ src/main/scala/com/github/rexim/Core.scala
@@ -8,55 +8,17 @@ case class Functor(name: String, args: List[Term]) extends Term
 case class Predicate(head: Functor, body: List[Functor])
 
 object LoprogContext {
-  private var varNameCount = 0
-
-  def generateVarName = {
-    varNameCount += 1
-    s"_G$varNameCount"
+  def unify(
+    left: Term,
+    right: Term,
+    bindings: Map[String, Term]
+  ): Boolean = (left, right) match {
+    case (Atom(leftName), Atom(rightName)) => leftName == rightName
+    case (atom: Atom, Variable(varName)) =>
+      if(bindings.contains(varName)) unify(atom, bindings.get(varName))
+      else true
+    case (variable: Variable, atom: Atom) => unify(atom, variable)
   }
-
-  def unify(left: Term, right: Term): Map[String, Term] =
-    (left, right) match {
-      case (Atom(leftName), Atom(rightName))
-          if leftName == rightName => Map()
-
-      case (Variable(leftVarName), Variable(rightVarName)) => {
-        val sharedVarName = generateVarName
-        Map(
-          leftVarName -> Variable(sharedVarName),
-          rightVarName -> Variable(sharedVarName)
-        )
-      }
-
-      case (Variable(varName), something) => Map(varName -> something)
-
-      case (something, Variable(varName)) => Map(varName -> something)
-
-      // FIXME: do it functional way
-      case (Functor(leftName, leftArgs), Functor(rightName, rightArgs))
-          if leftName == rightName && leftArgs.size == rightArgs.size => {
-            var result = Map[String, Term]()
-
-            for((left, right) <- leftArgs.zip(rightArgs)) {
-              val bindings = unify(left, right)
-              if(bindings != null) {
-                for((key, value) <- bindings) {
-                  if(!result.contains(key)) {
-                    result = result + (key -> value)
-                  } else if(value != result(key)) {
-                    return null
-                  }
-                }
-              } else {
-                return null
-              }
-            }
-
-            result
-          }
-
-      case _ => null
-    }
 }
 
 class LoprogContext(predicates: List[Predicate]) {
