7e1576d35f1e1fc31d28e132db78aabb77c777fe
rexim <reximkut@gmail.com>
2013-03-20 21:13:55 +0700

    Added .gitignore

--- src/main/scala/com/github/rexim/Core.scala
+++ src/main/scala/com/github/rexim/Core.scala
@@ -14,10 +14,23 @@ object LoprogContext {
     bindings: Map[String, Term]
   ): Boolean = (left, right) match {
     case (Atom(leftName), Atom(rightName)) => leftName == rightName
+
     case (atom: Atom, Variable(varName)) =>
       if(bindings.contains(varName)) unify(atom, bindings.get(varName))
       else true
+
     case (variable: Variable, atom: Atom) => unify(atom, variable)
+
+    case (Variable(leftName), Variable(rightName)) =>
+      if(bindings.contains(leftName) && bindings.contains(rightName)) {
+        unify(bindings(leftName), bindings(rightName))
+      } else {
+        true
+      }
+
+    // ...
+
+    case _ => false
   }
 }
 
