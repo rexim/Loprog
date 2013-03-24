94cce47fa91ed682ff6a37c5bbb71d69abd2c7de
rexim <reximkut@gmail.com>
2013-03-24 16:52:52 +0700

    Added test for Loprog.visitSolutions.

--- src/test/scala/LoprogSuite.scala
+++ src/test/scala/LoprogSuite.scala
@@ -1,4 +1,5 @@
 import org.scalatest.FunSuite
+import scala.collection.mutable.ListBuffer
 import com.github.rexim._
 
 class LoprogSuite extends FunSuite {
@@ -58,5 +59,61 @@ class LoprogSuite extends FunSuite {
       case None =>
         assert(false, s"$left doesn't unify $right")
     }
+  }
+
+  test("visitSolutions: a simple query on a program.") {
+    val program = List(
+      // p(a).
+      Predicate(Functor("p", List(Atom("a"))), List()),
+
+      // p(X) :- q(X), r(X).
+      Predicate(Functor("p", List(Variable("p1::X"))),
+        List(
+          Functor("q", List(Variable("p1::X"))),
+          Functor("r", List(Variable("p1::X")))
+        )),
+
+      // p(X) :- u(X).
+      Predicate(Functor("p", List(Variable("p2::X"))),
+        List(
+          Functor("u", List(Variable("p2::X")))
+        )),
+
+      // q(X) :- s(X).
+      Predicate(Functor("q", List(Variable("q1::X"))),
+        List(
+          Functor("s", List(Variable("q1::X")))
+        )),
+
+      // r(a).
+      Predicate(Functor("r", List(Atom("a"))), List()),
+
+      // r(b).
+      Predicate(Functor("r", List(Atom("b"))), List()),
+
+      // s(a).
+      Predicate(Functor("s", List(Atom("a"))), List()),
+
+      // s(b).
+      Predicate(Functor("s", List(Atom("b"))), List()),
+
+      // s(c).
+      Predicate(Functor("s", List(Atom("c"))), List()),
+
+      // u(d).
+      Predicate(Functor("u", List(Atom("d"))), List())
+    )
+
+    // ?- p(X).
+    val query = List(Functor("p", List(Variable("X"))))
+
+    val solutions = new ListBuffer[Map[String, Term]]()
+
+    Loprog.visitSolutions(program, query,
+      (bindings: Map[String, Term]) => {
+        solutions.append(bindings)
+      }, Map())
+
+    assert(solutions.toList.map(_.get("X")) == List(Some(Atom("a")), Some(Atom("a")), Some(Atom("b")), Some(Atom("d"))))
   }
 }
