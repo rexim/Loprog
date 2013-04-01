4ec85a833d99851876d411e2aec9fa65899937df
rexim <reximkut@gmail.com>
2013-04-01 16:26:18 +0700

    UtilsSuite: implemented all unimplemented tests. :)

--- src/test/scala/UtilsSuite.scala
+++ src/test/scala/UtilsSuite.scala
@@ -69,15 +69,58 @@ class UtilsSuite extends FunSuite {
     assert(result == answer)
   }
 
-  test("mapVarNam") {
-    assert(false, "Not implemented yet")
+  test("mapVarName") {
+    // foo(X, bar(Y), baz)
+    val term = Functor("foo",
+      List(
+        Variable("X"),
+        Functor("bar", List(Variable("Y"))),
+        Functor("baz", List())
+      )
+    )
+
+    // foo(X::derp, bar(Y::derp), baz)
+    val answer = Functor("foo",
+      List(
+        Variable("X::derp"),
+        Functor("bar", List(Variable("Y::derp"))),
+        Functor("baz", List())
+      )
+    )
+
+    val result = Utils.mapVarName(term, _ + "::derp")
+
+    assert(answer === result)
   }
 
   test("createGenerator") {
-    assert(false, "Not implemented yet")
+    val generator = Utils.createGenerator
+    assert(generator() === "_G1")
+    assert(generator() === "_G2")
+    assert(generator() === "_G3")
   }
 
   test("scopePredicate") {
-    assert(false, "Not implemented yet")
+    // foo(X, Y) :-
+    //   bar(Z), baz(X, Z, Y).
+    val predicate = Predicate(Functor("foo", List(Variable("X"), Variable("Y"))),
+      List(
+        Functor("bar", List(Variable("Z"))),
+        Functor("baz", List(Variable("X"), Variable("Z"), Variable("Y")))
+      )
+    )
+
+    // foo(_G1, _G2) :-
+    //   bar(_G3), baz(_G1, _G3, _G2).
+    val answer = Predicate(Functor("foo", List(Variable("_G1"), Variable("_G2"))),
+      List(
+        Functor("bar", List(Variable("_G3"))),
+        Functor("baz", List(Variable("_G1"), Variable("_G3"), Variable("_G2")))
+      )
+    )
+
+    val generator = Utils.createGenerator
+    val result = Utils.scopePredicate(predicate, generator)
+    assert(result === answer)
   }
 }
